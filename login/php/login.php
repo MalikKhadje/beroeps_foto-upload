c70a20b99d761f9c0e6f646bed41dc3efa984651
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-06 16:00:37 +0100

    DOE HET

--- login/php/login.php
+++ login/php/login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT ID, Naam, Wachtwoord FROM User WHERE Naam = foto_upload";
+        $sql = "SELECT ID, Naam, Wachtwoord FROM User WHERE Naam = ?";
         
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
