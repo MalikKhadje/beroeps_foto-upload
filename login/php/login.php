8714211fb6016a05f434e222c9008970375b40cd
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-13 13:01:27 +0100

    wewdsf

--- login/php/login.php
+++ login/php/login.php
@@ -108,7 +108,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }        
         ?>
 
-        <form action="loginverwerk.php <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="wrap">
                 <label class="label">NAAM</label><br>
                 <input type="text" name="username"  placeholder="NAAM" class="form-control input <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
