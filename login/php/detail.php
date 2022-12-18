6fc4adfcc5378c9eaca1fcd081d7d48a8b09c74b
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-18 13:48:20 +0100

    Update detail.php

--- login/php/detail.php
+++ login/php/detail.php
@@ -13,42 +13,35 @@
 require_once 'session.inc.php';
 require 'config.php';
 
-$id = $_GET['id'];
+// $id = $_GET['id'];
 
 // toon het id op het scherm
 echo "ID van het agenda-item is: " . $id . "<br/>";
 
 // maak de query om gegevens van het item op te halen
-$query = "SELECT * FROM crud_agenda WHERE ID = " . $id;
+$query = "SELECT * FROM users WHERE ID = " . $id;
 
 // voer de query uit
 $result = mysqli_query($mysqli, $query);
 
-if (mysqli_num_rows($result) > 0)
-{
-    // er hoeft maar 1 item uitgelezen te worden
-    $item = mysqli_fetch_assoc($result);
+// if (mysqli_num_rows($result) > 0)
+// {
+//     // er hoeft maar 1 item uitgelezen te worden
+//     $item = mysqli_fetch_assoc($result);
 
-    echo $item['Onderwerp'] . "<br/>";
-    echo $item['Inhoud'] . "<br/>";
-    echo $item['Begindatum'] . "<br/>";
-    echo $item['Einddatum'] . "<br/>";
-    echo $item['Prioriteit'] . "<br/>";
-    echo $item['Status'] . "<br/>";
-}
+//     echo $item['Onderwerp'] . "<br/>";
+//     echo $item['Inhoud'] . "<br/>";
+//     echo $item['Begindatum'] . "<br/>";
+//     echo $item['Einddatum'] . "<br/>";
+//     echo $item['Prioriteit'] . "<br/>";
+//     echo $item['Status'] . "<br/>";
+// }
 
-// als er niks gevonden is
-else
-{
-    echO "Er is geen record met ID: " . $id . "<br/>";
-}
-
-// verwijder het item
-echo "<a href='verwijder.php?id=" . $item['ID'] . "'>Verwijder</a><br/>";
-// echo . "<a href='verwijder.php?id= . $item['ID'] . '>Verwijder</a><br/>";
-
-// terug naar het overzicht
-// echo "<a href='toonagenda.php'>OVERZICHT</a><br/>";
+// // als er niks gevonden is
+// else
+// {
+//     echO "Er is geen record met ID: " . $id . "<br/>";
+// }
 
 echo "<a href='logout.php'>LOG UIT</a>";
 ?>
