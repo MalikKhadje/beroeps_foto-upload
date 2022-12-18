502e1a265171fc609a36e73e8df92e11e72819ed
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-18 15:24:40 +0100

    Update poep.php

--- login/php/poep.php
+++ login/php/poep.php
@@ -14,16 +14,6 @@ $query = "SELECT * FROM Posts";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./css/overzicht.css">
-    <link rel="stylesheet" href="./fonts/icomoon/style.css">
-    <link rel="stylesheet" href="./css/owl.carousel.min.css"> -->
-<!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="./css/bootstrap.min.css"> -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
 
     <title>Overzicht</title>
 
@@ -31,68 +21,54 @@ $query = "SELECT * FROM Posts";
 
 <h1>OVERZICHT</h1>
 
+<center>
+    <form action="" method="post" enctype="multipart/form-data">
+        <table width="50%" cellpadding="5" cellspacing="5" border="1">
+            <thead>
+                <tr>
+                    <th>Foto</th>
+                    <th>Titel</th>
+                    <th>Beschrijving</th>
+                    <th>Aangemaakt Op</th>
+                </tr>
+            </thead>
+            <?php
+            $connection = mysqli_connect("localhost", "foto_upload", "foto_upload");
+            $db = mysqli_select_db($connection, 'foto_upload');
+
+            $query = " SELECT * FROM `Posts` ";
+            $query_run = mysqli_query($connection, $query);
+
+            while($row = mysqli_fetch_array($query_run))
+            {
+                ?>
+                <tr>
+                    <td> <?php echo '<img src="data:image;base64,'.base64_encode($row['image']).'" alt="Image" style="width: 100px; height: 100px" >'; ?> </td>
+                    <td> <?php echo $row['Titel'] ?> </td>
+                    <td> <?php echo $row['Beschrijving'] ?> </td>
+                    <td> <?php echo $row['Aangemaakt_op'] ?> </td>
+                    <td> <?php echo '<a href="detail.php?id=' . $row['ID'] . '">Detail</a>' ?>  </td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+    </form>
+</center>
+
 
 <?php
 
 
-$result = mysqli_query($mysqli, $query);
-
-if (mysqli_num_rows($result) > 0)
-{
-    // maak een tabel aan
-    echo "<table border='1px' class='table table-striped custom-table'>";
-
-    // eerste de headers van de tabel
-    echo "<tr>
-    <th>Foto</th>
-    <th>Titel</th>
-    <th>Beschrijving</th>
-    <th>Aangemaakt Op</th>
-    </tr>";
-
-    while ($item = mysqli_fetch_assoc($result))
-
-    {
-      ?>
-        <?php
-        // toon de gegevens van het item in een tabelrij
-        echo "<tr>";
-            //  echo "<td>" . $item['Foto'] . "</td>";
-             echo "<td>" . $item['Titel'] . "</td>";
-             echo "<td>" . $item['Beschrijving'] . "</td>";
-             echo "<td>" . $item['Aangemaakt_op'] . "</td>";
-             echo "<td>" . "<a href='detail.php?id=" . $item['ID'] . "'>Detail</a> </td>";
-        echo "</tr>";
-        ?>
-        <?php
-    }
-
-    // sluit de tabel af
-    echo "</table>";
-}
-
-$result = mysqli_query($mysqli, $query);
-
-if (!$result)
-// laat de records zien
-{
-    echo "<p>FOUT!</p>";
-    echo "<p>" . $query ."</p>";
-    echo "<p>" . mysqli_error($mysqli) . "</p>";
-    exit;
-}
-
-// else
-// // als er geen records zijn
-// {
-//     echo "<p>Geen items gevonden!</p>";
-    
-// }
 
 echo "<a href='logout.php'>LOG UIT</a>";
 
 
 ?>
+
+
+
+
 
 
 
