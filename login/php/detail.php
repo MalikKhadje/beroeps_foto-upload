9cfcad91cc7539f3e68dc206883cfe262e60b567
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-19 14:16:55 +0100

    lol

--- login/php/detail.php
+++ login/php/detail.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/detail.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="../js/script.js" defer></script>i
+    <script src="../js/script.js" defer></script>
     <title>Detail</title>
 
 </head>
