f01067b68dd15262e15fd561cea04a87f4bc81b2
Malik <87253@glr.nl>
2022-12-19 16:12:36 +0100

    save

--- Home/Home.js
+++ Home/Home.js
@@ -39,7 +39,7 @@ const autoSlide = () => {
 }
 
 const dragStart = (e) => {
-    // updatating global variables value on mouse down event
+    // updatating global letiables value on mouse down event
     isDragStart = true;
     prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
@@ -94,4 +94,36 @@ function myFunction() {
 }
 
 
-// ************************************************************* ...
+// ************************************************************* MODAL
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+
+const images = document.getElementsByTagName('img');
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    });
+}
+
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+images.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
