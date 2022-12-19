8e277806806f130a8c61ee199286a96d522dd13c
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-19 14:35:19 +0100

    gdgfdg

--- login/js/script.js
+++ login/js/script.js
@@ -1,6 +1,6 @@
 function myFunction() {
 
-    let elements = document.querySelectorAll('div, button, nav, a, p, body, html, footer, i, input, span, :before, :after');
+    let elements = document.querySelectorAll('div, button, nav, a, p, body, html, footer, i, input, span, :before, :after, b, h1');
 
     for (let i = 0; i < elements.length; i++) {
 
