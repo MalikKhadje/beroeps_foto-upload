f970d0d92d894289077b8940ea723396d2b31798
Malik <87253@glr.nl>
2022-12-19 15:39:38 +0100

    save

--- Home/Home.js
+++ Home/Home.js
@@ -86,7 +86,7 @@ carousel.addEventListener("touchend", dragStop);
 // }
 
 function myFunction() {
-    let elements = document.querySelectorAll('div, button, nav, a, p, body, html, footer, i, input, span, :before, :after');
+    let elements = document.querySelectorAll('div, button, nav, a, p, body, html, footer, i, input, span, :before, :after, b, h1');
 
     for (let i = 0; i < elements.length; i++) {
         elements[i].classList.toggle("dark-mode");
