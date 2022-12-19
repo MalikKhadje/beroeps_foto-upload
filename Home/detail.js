69ee070e8993fba480e26e41a318c54af1870b51
Malik <malikkhadje@gmail.com>
2022-12-19 23:42:47 +0100

    Home gelinked

--- Home/detail.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    let elements = document.querySelectorAll('div, button, nav, a, p, body, html, footer, i, input, span, :before, :after, b, h1');
-
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle("dark-mode");
-    }
-}
