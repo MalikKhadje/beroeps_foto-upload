009d3d561d832f2a6eb9b01016a1c5fff8c62f97
deaszy <brampeer15@gmail.com>
2022-12-19 23:50:18 +0100

    favicon uploadpage

--- uploadpagina/fotoupload.php
+++ uploadpagina/fotoupload.php
@@ -19,6 +19,7 @@ if (isset($_SESSION["token"]) && $_SESSION["token"] == $_POST["csrf_token"]) {
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
 </head>
 <body>
 <button onclick="myFunction()" id="dark-mode" class="fa fa-moon-o w3-circle"></button>
