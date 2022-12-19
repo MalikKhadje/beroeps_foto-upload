1a5d2405b6f6024e27ceaa484865075b49b3c01c
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-19 13:25:57 +0100

    hehehehe

--- login/php/detail.php
+++ login/php/detail.php
@@ -6,14 +6,14 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/detail.css">
-    
+
     <title>Detail</title>
 
 </head>
 
 <div id="logo">
-        <img src="../images/logo.png" class="logo" alt="">
-    </div>
+    <img src="../images/logo.png" class="logo" alt="">
+</div>
 <h1 class="titel">LOGIN</h1>
 
 <?php
@@ -33,22 +33,32 @@ $query = "SELECT * FROM Posts WHERE ID = " . $id;
 // voer de query uit
 $result = mysqli_query($mysqli, $query);
 
-if (mysqli_num_rows($result) > 0)
-{
+if (mysqli_num_rows($result) > 0) {
     // er hoeft maar 1 item uitgelezen te worden
     $item = mysqli_fetch_assoc($result);
+?>
+<div class="wrap">
+    <?php echo '<img src="data:image;base64,' . base64_encode($item['Foto']) . '" alt="Image" style="width: 100px; height: 100px">'; ?><br>
+</div>
 
-    echo '<img src="data:image;base64,'.base64_encode($item['Foto']).'" alt="Image" style="width: 100px; height: 100px">';?><br><?php
-    echo $item['Titel'] . "<br/>";
-    echo $item['Beschrijving'] . "<br/>";
-    echo $item['Aangemaakt_op'] . "<br/>";
+<div class="wrap">
+    <?php echo $item['Titel'] . "<br/>"; ?>
+</div>
+
+<div class="wrap">
+    <?php echo $item['Beschrijving'] . "<br/>"; ?>
+</div>
+
+<div class="wrap">
+    <?php echo $item['Aangemaakt_op'] . "<br/>"; ?>
+</div>
+<?php
 }
 
 // als er niks gevonden is
-else
-{
+else {
     echo "Er is geen record met ID: " . $id . "<br/>";
 }
 
 echo "<a href='logout.php'>LOG UIT</a>";
-?>
+    ?>
